2b1b1ccbe47a63d8a8c685ba8ed4580a630cc6ea
maperperdaw <maperperdaw@gmail.com>
2023-10-14 11:12:53 +0200

    Exercise 1.1

---
+++ part1/src/App.tsx
@@ -0,0 +1,15 @@
+import Content from "./component/Content"
+import Header from "./component/Header";
+
+const App = () => {
+  return (
+    <>
+      <div>
+        <Header/>
+        <Content />
+      </div>
+    </>
+  )
+}
+
+export default App;
---
+++ part1/src/component/Content.tsx
@@ -0,0 +1,27 @@
+import Total from "./Total"
+
+const Content = () => {
+
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+  return (
+    <>
+        <p>
+          {part1} {exercises1}
+        </p>
+        <p>
+          {part2} {exercises2}
+        </p>
+        <p>
+          {part3} {exercises3}
+        </p>
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/> 
+    </>
+  );
+}
+
+export default Content;
